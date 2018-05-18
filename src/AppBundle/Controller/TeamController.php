ae0d326395192d52052044385a7a2dabd126f3da
julien hoste <hostejulien@gmail.com>
2018-05-18 10:01:41 +0200

    ajout noms pays

--- src/AppBundle/Controller/TeamController.php
+++ src/AppBundle/Controller/TeamController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\Team;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Intl\Intl;
 
 /**
  * Team controller.
@@ -23,12 +24,46 @@ class TeamController extends Controller
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
-
+        $tab=array();
         $teams = $em->getRepository('AppBundle:Team')->findAll();
+        foreach ($teams as $key =>  $team) {
+            $tab[$key]['team']=$team;
+            $tab[$key]['country']=Intl::getRegionBundle()->getCountryName($team->getCountry());
+        }
+
 
         return $this->render('team/index.html.twig', [
             'teams' => $teams,
         ]);
+        return $this->render('team/index.html.twig', array(
+            'tabs' => $tab,
+        ));
+    }
+
+    /**
+     * Creates a new team entity.
+     *
+     * @Route("/new", name="admin_team_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $team = new Team();
+        $form = $this->createForm('AppBundle\Form\TeamType', $team);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($team);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_team_show', array('id' => $team->getId()));
+        }
+
+        return $this->render('team/new.html.twig', array(
+            'team' => $team,
+            'form' => $form->createView(),
+        ));
     }
 
     /**
